bf7bf0e42d0ef26a732478c2188b92e656691032
MONIKA SHARMA <63012770+monika200@users.noreply.github.com>
2020-05-03 17:32:48 +0530

    Add files via upload

---
+++ python4.py
@@ -0,0 +1,15 @@
+# Python program to demonstrate    
+# Removal of elements in a List   
+      
+# Creating a List   
+List = [1, 2, 3, 4, 5, 6,    
+        7, 8, 9, 10, 11, 12] 
+  
+# using Remove() method   
+List.remove(5)   
+List.remove(6) 
+print(List)   
+    
+# using pop() 
+List.pop() 
+print(List)
